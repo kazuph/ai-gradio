01e196dc002caedcb08b21aa3b5f17b6c175d2ef
AK391 <ahsen@huggingface.co>
2025-01-03 22:44:25 -0500

    make readme more concise

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import gradio as gr
 import ai_gradio
 
 gr.load(
-    name='groq:llama-3.2-11b-vision-preview',
+    name='deepseek:deepseek-chat',
     src=ai_gradio.registry,
-    coder=True
 ).launch()
 
