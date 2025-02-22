9727ca68f7672bb2e465cec64adabdaddb3b736d
Kazuhiro Homma <kazu.homma@gmail.com>
2025-02-22 23:21:06 +0900

    add js

---
+++ ai-gradio-js/vite.config.ts
@@ -0,0 +1,24 @@
+import { reactRouter } from '@react-router/dev/vite'
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { getLoadContext } from './load-context'
+
+export default defineConfig({
+  plugins: [
+    cloudflareDevProxy({ getLoadContext }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ['workerd', 'worker', 'browser'],
+    },
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+  },
+  build: {
+    minify: true,
+  },
+})
